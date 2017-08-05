26b84c12a4f81725e6047ba165d2b52e3948baf1
Daniel <daniel.rasinski@yahoo.com>
2017-08-05 13:29:04 -0500

    udpated for react-native to use fore browser use

--- src/request/request.js
+++ src/request/request.js
@@ -3,4 +3,4 @@ var isBrowser = typeof window !== 'undefined';
 // FIXME: how to do conditional loading this with ES6 modules?
 module.exports = isBrowser ?
     require('./request.browser') :
-    require('./request.node');
+    require('./request.browser');
